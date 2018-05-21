56b139eb48c5d95891eb48788a3aa879251c2e4d
almartin82 <almartin@nps.k12.nj.us>
2018-05-21 16:56:50 -0400

    determine file type per file.  closes #3

--- R/parccvizieR.R
+++ R/parccvizieR.R
@@ -4,10 +4,13 @@
 #' \code{parccvizieR} is the primary workflow function that generates the
 #' parccvizieR object
 #'
-#' @param results a PARCC roster, or summative file
+#' @param results_location a file, or directory of files, containing PARCC
+#' results.  parccvizieR supports NJ summative record files, but is
+#' architected to gracefully extend support to other PARCC consortium states
 #' @param local_roster optional.  additional roster data to supplement
 #' the data in the PARCC file.
-#' @param ... additional arguments
+#' @param verbose print status updates as the data is processed?
+#' default is TRUE
 #' @examples
 #'\dontrun{
 #' pviz <- parccvizieR(sf = ex_sf)
@@ -16,7 +19,7 @@
 #' }
 #' @export
 
-parccvizieR <- function(results, local_roster = NA, verbose = FALSE, ...) {
+parccvizieR <- function(results, local_roster = NA, verbose = TRUE) {
   UseMethod("parccvizieR")
 }
 
@@ -24,63 +27,44 @@ parccvizieR <- function(results, local_roster = NA, verbose = FALSE, ...) {
 parccvizieR.default <- function(results, local_roster = NA, verbose = FALSE, ...) {
 
   #read in results.  get a list of one OR MORE paths to files.
-
-  #for EACH FILE, determine the layout
-
-  #use map2df on the list of files and list of layouts to process the data
-
-
   raw <- read_raw_results(results)
 
-  #given the names of the raw file, detect the type
-  result_type <- detect_result_file_origin(names(raw))
+  #for EACH FILE, determine the layout
+  file_origins <- map_chr(
+    raw,
+    detect_result_file_origin
+  )
 
-  if (identical(result_type, character(0))) {
-    stop("Unknown PARCC results type.")
-
-  # NJ_SRF is supported.
-  } else if (identical(result_type, 'NJ_SRF')) {
-    message('Detected NJ PARCC Summative Record File')
-
-    #process NJ_SRF
-
-  # stub. left as an exercise for future development / contributors
-  } else if (identical(result_type, 'PARCC')) {
-    message('Detected Generic PARCC Summative File')
-    message('Please write a processing function for me :)')
-
-    #TODO: process Generic PARCC Summative File data file
-
-  # stub. left as an exercise for future development / contributors
-  } else if (identical(result_type, 'LA')) {
-    message('Detected Louisiana Roster file')
-    message('Please write a processing function for me :)')
-
-    #TODO: process louisiana data file
-  }
+  #use map2df on the list of files and list of layouts to process the data
 
   #return parccvizieR object
   out <- NA
   class(out) <- "parccvizieR"
-
-  out
 }
 
 
+#' Title
+#'
+#' @param x the results argument from parccvizieR.  either a path to one file
+#' OR a folder of files
+#'
+#' @return character vector of full paths to results files
+#' @export
+
 read_raw_results <- function(x) {
 
-  #if csv is in the results argument, assume that we have a FILE
+  #if we can find 'dot csv' in the results argument,
+  #assume that we have a path to a SINGLE FILE and return that.
   if (grepl('.csv', x, ignore.case = TRUE)) {
-    raw_path <- x
-  #otherwise assume that we have a PATH, return the FIRST csv in that folder
-  #this is just to detect the format of the results file we are looking at
+    out <- file.path(dirname(x), basename(x))
+  #otherwise assume we have a directory.  in that case, return a vector of
+  #paths.
   } else {
-    raw_path <- dir(path = x, pattern = "csv", ignore.case = TRUE,
-        recursive = TRUE, full.names = TRUE)[1]
+    out <- dir(path = x, pattern = "csv", ignore.case = TRUE,
+        recursive = TRUE, full.names = TRUE)
   }
 
-  #read the raw path
-  suppressMessages(suppressWarnings(readr::read_csv(file = raw_path)))
+  out
 }
 
 
@@ -92,26 +76,17 @@ read_raw_results <- function(x) {
 #' function detects the type of file by looking at the names/columns, and
 #' processes accordingly.
 #'
-#' @param df
+#' @param df_path
 #'
 #' @return character string representing type detected
 #' @export
 
-detect_result_file_origin <- function(df) {
+detect_result_file_origin <- function(df_path) {
 
-  LA <- c("District.Code", "District.Name", "School.Code", "School.Name",
-    "Last.Name", "First.Name", "Middle.Initial", "Student.ID", "Grade",
-    "Gender", "Ethnicity", "Education.Classification", "Economically.Disadvantaged.Status",
-    "Limited.English.Proficient", "Migrant", "Section.504", "ELA.Scale.Score",
-    "ELA.Achievement.Level", "ELA.Reading.Performance", "ELA.Writing.Performance",
-    "ELA.Informational.Text", "Reading.Literary.Text", "Reading.Vocabulary",
-    "Written.Expression", "Written.Knowledge...Use.of.Language.Convention",
-    "Math.Scale.Score", "Math.Achievement.Level", "Major.Content",
-    "Expressing.Mathematical.Reasoning", "Modeling...Application",
-    "Additional...Supporting.Content")
+  df_names <- suppressMessages(suppressWarnings(readr::read_csv(df_path))) %>%
+    names()
 
-  #from 2015, using a sample / released data file.  out of date?
-  PARCC <- c("recordType", "multipleRecordFlag", "reportedSummativeScoreFlag",
+  NJ_SRF15 <- c("recordType", "multipleRecordFlag", "reportedSummativeScoreFlag",
     "reportedRosterFlag", "reportSuppressionCode", "reportSuppressionAction",
     "assessmentYear", "pba03Category", "eoy03Category", "stateAbbreviation",
     "responsibleDistrictIdentifier", "responsibleDistrictName", "responsibleSchoolInstitutionIdentifier",
@@ -162,12 +137,71 @@ detect_result_file_origin <- function(df) {
     "eoyUnit4TotalNumberOfItems", "eoyUnit4NumberOfAttemptedItems",
     "eoyUnit5TotalNumberOfItems", "eoyUnit5NumberOfAttemptedItems",
     "pbaNotTestedReason", "eoyNotTestedReason", "pbaVoidPbaEoyScoreReason",
-    "eoyVoidPbaEoyScoreReason", "filler.1", "filler.2", "summativeScaleScore",
+    "eoyVoidPbaEoyScoreReason", "filler_1", "filler_2", "summativeScaleScore",
     "summativeCsem", "summativePerformanceLevel", "summativeReadingScaleScore",
     "summativeReadingCsem", "summativeWritingScaleScore", "summativeWritingCsem",
     "subclaim1Category", "subclaim2Category", "subclaim3Category",
     "subclaim4Category", "subclaim5Category", "subclaim6Category",
-    "filler.3", "filler.4", "filler.5")
+    "filler_3", "filler_4", "filler_5")
+
+  NJ_SRF16 <- c("StateAbbreviation", "TestingDistrictCode", "TestingSchoolCode",
+    "ResponsibleDistrictCode", "ResponsibleSchoolCode", "StateStudentIdentifier",
+    "LocalStudentIdentifier", "PARCCStudentIdentifier", "LastOrSurname",
+    "FirstName", "MiddleName", "Birthdate", "Sex", "StateField1",
+    "GradeLevelWhenAssessed", "HispanicOrLatinoEthnicity", "AmericanIndianOrAlaskaNative",
+    "Asian", "BlackOrAfricanAmerican", "NativeHawaiianOrOtherPacificIslander",
+    "White", "FillerField", "TwoOrMoreRaces", "EnglishLearnerEL",
+    "TitleIIILimitedEnglishProficientParticipationStatus", "GiftedandTalented",
+    "MigrantStatus", "EconomicDisadvantageStatus", "StudentWithDisabilities",
+    "PrimaryDisabilityType", "StateField2", "StateField3", "StateField4",
+    "StateField5", "StateField6", "StateField7", "StateField8", "StateField9",
+    "StateField10", "StateField11", "StateField12", "Filler", "ClassName",
+    "TestAdministrator", "StaffMemberIdentifier", "TestCode", "Filler_1",
+    "Retest", "ELAccommodation", "FrequentBreaks", "SeparateAlternateLocation",
+    "SmallTestingGroup", "SpecializedEquipmentOrFurniture", "SpecifiedAreaOrSetting",
+    "TimeOfDay", "AnswerMasking", "FillerField_1", "ColorContrast",
+    "ASLVideo", "AssistiveTechnologyScreenReader", "AssistiveTechnologyNonScreenReader",
+    "ClosedCaptioningForELAL", "RefreshableBrailleDisplayForELAL",
+    "AlternateRepresentationPaperTest", "LargePrint", "BrailleWithTactileGraphics",
+    "StudentReadsAssessmentAloudToThemselves", "HumanSignerForTestDirections",
+    "AnswersRecordedInTestBook", "BrailleResponse", "CalculationDeviceAndMathematicsTools",
+    "ELALConstructedResponse", "ELALSelectedResponseOrTechnologyEnhancedItems",
+    "MathematicsResponse", "MonitorTestResponse", "WordPrediction",
+    "AdministrationDirectionsClarifiedinStudentsNativeLanguage",
+    "AdministrationDirectionsReadAloudinStudentsNativeLanguage",
+    "MathematicsResponseEL", "TranslationOfTheMathematicsAssessment",
+    "WordtoWordDictionaryEnglishNativeLanguage", "TextToSpeech",
+    "HumanReaderOrHumanSigner", "UniqueAccommodation", "EmergencyAccommodation",
+    "ExtendedTime", "StudentTestUUID", "PaperFormID", "OnlineFormID",
+    "TestStatus", "TotalTestItems", "TestAttemptednessFlag", "TotalTestItemsAttempted",
+    "PaperUnit1TotalTestItems", "PaperSection1NumberofAttemptedItems",
+    "PaperSection2TotalTestItems", "PaperSection2NumberofAttemptedItems",
+    "PaperSection3TotalTestItems", "PaperSection3NumberofAttemptedItems",
+    "PaperSection4TotalTestItems", "PaperSection4NumberofAttemptedItems",
+    "StudentUnit1TestUUID", "Unit1FormID", "Unit1TotalTestItems",
+    "Unit1NumberofAttemptedItems", "StudentUnit2TestUUID", "Unit2FormID",
+    "Unit2TotalTestItems", "Unit2NumberOfAttemptedItems", "StudentUnit3TestUUID",
+    "Unit3FormID", "Unit3TotalTestItems", "Unit3NumberOfAttemptedItems",
+    "StudentUnit4TestUUID", "Unit4FormID", "Unit4TotalTestItems",
+    "Unit4NumberofAttemptedItems", "NotTestedCode", "NotTestedReason",
+    "VoidScoreCode", "VoidScoreReason", "ShipReportDistrictCode",
+    "ShipReportSchoolCode", "Summative Flag", "MultipleTestRegistration",
+    "RosterFlag", "ReportSuppressionCode", "ReportSuppressionAction",
+    "AttemptCreateDate", "Unit1OnlineTestStartDateTime", "Unit1OnlineTestEndDateTime",
+    "Unit2OnlineTestStartDateTime", "Unit2OnlineTestEndDateTime",
+    "Unit3OnlineTestStartDateTime", "Unit3OnlineTestEndDateTime",
+    "Unit4OnlineTestStartDateTime", "Unit4OnlineTestEndDateTime",
+    "AssessmentYear", "AssessmentGrade", "Subject", "FederalRaceEthnicity",
+    "Period", "TestingOrganizationalType", "TestingDistrictName",
+    "TestingSchoolName", "ResponsibleOrganizationCodeType", "ResponsibleDistrictName",
+    "ResponsibleSchoolName", "Filler_2", "Filler_3", "Filler_4",
+    "Filler_5", "Filler_6", "Filler_7", "TestScaleScore", "TestCSEMProbableRange",
+    "TestPerformanceLevel", "TestReadingScaleScore", "TestReadingCSEM",
+    "TestWritingScaleScore", "TestWritingCSEM", "Subclaim1Category",
+    "Subclaim2Category", "Subclaim3Category", "Subclaim4Category",
+    "Subclaim5Category", "Subclaim6Category", "Filler_8", "Filler_9",
+    "Filler_10", "Filler_11", "TestScoreComplete"
+  )
 
   NJ_SRF17 <- c("StateAbbreviation", "TestingDistrictCode", "TestingSchoolCode",
      "ResponsibleDistrictCode", "ResponsibleSchoolCode", "StateStudentIdentifier",
@@ -233,16 +267,24 @@ detect_result_file_origin <- function(df) {
      "FillerField7", "FillerField_28", "FillerField_29", "FillerField_30"
   )
 
-  results_files <- list('LA' = LA, 'PARCC' = PARCC, 'NJ_SRF' = NJ_SRF)
+  results_files <- list(
+    'NJ_SRF15' = NJ_SRF15, 'NJ_SRF16' = NJ_SRF16, 'NJ_SRF17' = NJ_SRF17
+  )
 
   mask <- vapply(
-    X = results_files, FUN = function(x) setequal(df, x), logical(1)
+    X = results_files, FUN = function(x) setequal(df_names, x), logical(1)
   )
   out <- names(results_files)[mask]
 
+  if (length(out) == 0) {
+    stop(paste0(
+      sprintf("Unable to determine file format for %s.", df_path), '\n',
+      "parccvizieR currently supports NJ SRF files for 2014-15", '\n' ,
+      "through 2017-18.  Please file an issue at:", '\n',
+      "https://github.com/almartin82/parccvizieR/issues", '\n',
+      "with any details of your result file (eg, State?)."
+    ))
+  }
+
   return(out)
-}
-
-
-read_raw_nj_srf <- function(x) {
 }
