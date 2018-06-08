2db8f699d8d36b98bc0730e4b3db08100d9b6138
Martin <a3martin@nps.k12.nj.us>
2018-06-08 11:57:43 -0400

    limit a SRF to given students

--- R/summary_stats.R
+++ R/summary_stats.R
@@ -1,3 +1,12 @@
-summary_stats <- function(pv, studentids, subject = NA, year = NA) {
+#' Calculate basic summary stats.  Useful for initial 'how did we do' cuts and questions.
+#'
+#' @inheritParams pv_limit_srf
+#'
+#' @return summary data frame
+#' @export
+
+basic_summary_stats <- function(
+  pv_obj, studentids, subject_area = NA, academic_year = NA
+) {
 
 }
--- R/util.R
+++ R/util.R
@@ -15,3 +15,28 @@
 cohort_year <- function(end_year, grade) {
   end_year + 1 + 12 - grade
 }
+
+
+#' Limit a parccvizieR SRF data frame
+#'
+#' @param pv_obj a conforming parccvizieR object
+#' @param studentids vector of state_student_identifiers
+#' @param subject_area optional, ELA or Math
+#' @param academic_year optional, ending YYYY
+
+pv_limit_srf <- function(pv_obj, studentids, subject_area = NA, academic_year = NA) {
+  #nse
+  subject_area_in <- subject_area
+
+  #extract the object
+  srf <- pv_obj$srf
+
+  #only these kids
+  out <- srf %>%
+    dplyr::filter(
+      studentid %in% studentids,
+      subject_area == subject_area_in
+    )
+
+  return(out)
+}
